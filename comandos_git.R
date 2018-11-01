0e5ac82560dea00c3007aff6205b0232bb814683
a-azevedolopes <amanda.azevedo4@gmail.com>
2018-11-01 16:39:34 -0300

    comandos para usar github com RStudio

---
+++ comandos_git.R
@@ -0,0 +1,6 @@
+# New File
+# Comandos do git:
+# $ git remote add origin "http://endereço do repositorio.git"
+# $ git config remote.origin.url git@github.com:a-azevedolopes/gitTest.git
+# $ git pull -u origin master
+# $ git push -u origin master
